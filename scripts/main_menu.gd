0b3ba1782eb667dbde1307114a77f6f8b4b9a761
mouna-ux <mounast007@gmail.com>
2025-07-11 14:09:40 +0100

    fixed font

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -5,21 +5,21 @@ func _ready() :
 	pass 
 
 
-func _process (detlta):
-	pass
+func _process(detlta):
+	pass 
 
 
-func _on_start_pressed() -> void:
+func _on_start_pressed():
 	get_tree().change_scene_to_file("res://complete scenes/Room.tscn")
 
 
-func _on_resume_pressed() -> void:
+func _on_resume_pressed():
 	print("Resume pressed")
 
 
-func _on_options_pressed() -> void:
+func _on_options_pressed():
 	print("Options pressed")
 
 
-func _on_exit_pressed() -> void:
+func _on_exit_pressed():
 	get_tree().quit()
